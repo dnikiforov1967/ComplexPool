8eefcf0b77a230e52dbb652c7f5015bcf60c0508
dnikiforov1967 <dnikiforov@mail.ru>
2015-03-08 11:41:51 +0300

    2 new problems added

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Stack;
 /**
  * Hello world!
  *
@@ -7,37 +8,13 @@ package org.example;
 public class App 
 {
 	
-	public static int pool(int[] A) {
-		int i = 0,j = A.length-1;
-		int volume = 0;
-		int left = 0, right = 0;
-		int wl =0;
-		while(i < j) {
-			//While left wall is less or equal to right one and until pointers become the same
-			while(i < j && A[i] <= right) {
-				if (A[i] <= wl) //Wall under water
-					volume += wl-A[i];
-				else
-					wl = A[i]; //Wall over water - add water to the edge
-				i++;
-			}
-			left = A[i]; wl = right; //Keep left and change whater level
-			//The same for right side - go to the left
-			while(j > i && A[j] <= left) {
-				if (A[j] <= wl) 
-					volume += wl-A[j];
-				else
-					wl = A[j]; 
-				j--;
-			}		
-			right = A[j]; wl = left;
-		}
-		return volume;
-		
-	}	
-	
     public static void main( String[] args )
     {
-        System.out.println( "Complex Pool Task" );
+		Stack<Integer> l = new Stack<>();
+		for(int i = 1; i<100000; i++) l.push(i);
+		l.push(100001);
+		int[] A = new int[l.size()];
+		for(int i=0; i<A.length; i++) A[i] = l.pop();
+        System.out.println(new MissingElement().solution(A));
     }
 }
