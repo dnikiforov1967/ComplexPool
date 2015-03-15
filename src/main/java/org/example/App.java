d5b7fa64557386e7970bf62023aacec05cbfba71
dnikiforov1967 <dnikiforov@mail.ru>
2015-03-16 00:05:33 +0300

    New tasks added

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,11 +10,6 @@ public class App
 	
     public static void main( String[] args )
     {
-		Stack<Integer> l = new Stack<>();
-		for(int i = 1; i<100000; i++) l.push(i);
-		l.push(100001);
-		int[] A = new int[l.size()];
-		for(int i=0; i<A.length; i++) A[i] = l.pop();
-        System.out.println(new MissingElement().solution(A));
+		System.out.println(new MinTwoAvgSlice().solution(new int[] {9, 9, 9, 1, 3, 1, 9}));
     }
 }
