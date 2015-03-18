9d093ba95372d374317b88d55c426087c24d034e
dnikiforov1967 <dnikiforov@mail.ru>
2015-03-19 00:07:33 +0300

    QSort modified

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,8 +10,8 @@ public class App
 	
     public static void main( String[] args )
     {
-		int[] A = new int[] {8,3,7,1,2,10,5};
-		new MergeSort().sort(A);
+		int[] A = new int[] {-3,-9,-5,-1,-2};
+		new QuickSort().sort(A);
 		for(int i=0; i<A.length; i++) 
 			System.out.println(A[i]);
     }
