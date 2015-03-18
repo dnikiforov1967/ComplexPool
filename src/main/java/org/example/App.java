25eae20cb48fb41469f475ca524e4eb52bc75c2d
dnikiforov1967 <dnikiforov@mail.ru>
2015-03-18 14:46:35 +0300

    MergeSort added

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,8 +10,8 @@ public class App
 	
     public static void main( String[] args )
     {
-		int[] A = new int[] {8,3,4,};
-		new HeapSort().sort(A);
+		int[] A = new int[] {8,3,7,1,2,10,5};
+		new MergeSort().sort(A);
 		for(int i=0; i<A.length; i++) 
 			System.out.println(A[i]);
     }
